cd31e6acb081568f6ff7f0ddd2875d2aab257163
Rodrigo Ayala <rayalac@yahoo.com>
2011-11-29 03:22:36 -0300

    First working version of ohloh strategy

--- lib/omniauth-ohloh.rb
+++ lib/omniauth-ohloh.rb
@@ -1,7 +1,2 @@
 require "omniauth-ohloh/version"
-
-module Omniauth
-  module Ohloh
-    # Your code goes here...
-  end
-end
+require "omniauth/strategies/ohloh"
---
+++ lib/omniauth/strategies/ohloh.rb
@@ -0,0 +1,37 @@
+require 'omniauth-oauth'
+require 'rexml/document'
+
+module OmniAuth
+  module Strategies
+    class Ohloh < OmniAuth::Strategies::OAuth
+
+      option :name, "ohloh"
+
+      option :client_options, {
+        :site => "http://www.ohloh.net",
+        :token_url => "/oauth/access_token"
+      }
+
+      uid{raw_info['id']}
+
+      info do
+        {
+          :name => raw_info['name'],
+          :location => raw_info['location'],
+          :image => raw_info['avatar_url']
+        }
+      end
+
+      extra do
+        {
+          'raw_info' => raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= Hash[REXML::Document.new(access_token.get('/accounts/me.xml').body).root.get_elements('/response/result/account').first.elements.collect{|n| [n.name,n.text]}]
+      end
+
+    end
+  end
+end
